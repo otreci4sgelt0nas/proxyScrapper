ae4a2a383e5cad9e39a64675c5100075e36fc776
otreci4sgelt0nas <96852897+otreci4sgelt0nas@users.noreply.github.com>
2023-06-21 10:52:33 +0100

    Update main.py
    This is more useful update, which tries whether the proxy is working or not. On google.com site if it works HTTP response code is 200 else not working. Additionally within response it will print response time, although the response time is limited to 5 seconds.

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+
+def test_proxy(proxy):
+    url = 'http://google.com'
+    proxies = {
+        'http': f'http://{proxy}',
+        'https': f'https://{proxy}'
+    }
+    
+    start_time = time.time()
+    
+    try:
+        response = requests.get(url, proxies=proxies, timeout=5)
+        if response.status_code == 200:
+            elapsed_time = time.time() - start_time
+            print(f"Proxy: {proxy}\tResponse Time: {elapsed_time:.2f} seconds")
+        else:
+            print(f"Proxy: {proxy}\tResponse: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Proxy: {proxy}\tError: {e}")
 
 def scrape_proxies(url):
     response = requests.get(url)
@@ -20,35 +40,8 @@ def scrape_proxies(url):
     return proxy_list
 
 # Example usage:
-url = 'import requests
-from bs4 import BeautifulSoup
-
-def scrape_proxies(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    proxy_list = []
-    
-    table = soup.find('table')
-    rows = table.find_all('tr')
-    
-    for row in rows[1:]:
-        columns = row.find_all('td')
-        ip = columns[0].text
-        port = columns[1].text
-        proxy = f"{ip}:{port}"
-        proxy_list.append(proxy)
-    
-    return proxy_list
-  
-# feel free to replace with own proxy sources, for this instance I decided to use sslproxies.org
 url = 'https://www.sslproxies.org'
 proxies = scrape_proxies(url)
 
 for proxy in proxies:
-    print(proxy)
-'
-proxies = scrape_proxies(url)
-
-for proxy in proxies:
-    print(proxy)
+    test_proxy(proxy)
