886f1fa3b0440f68d0bf81c3cb1c2ae16c063289
otreci4sgelt0nas <96852897+otreci4sgelt0nas@users.noreply.github.com>
2023-07-06 15:00:19 +0300

    Update main.py
    From the UX perspective realized it would be much easier for the end user to view working/not working proxies by adding a color distinction which is green for working and red for not working. Now it is much easier to view.

--- main.py
+++ main.py
@@ -2,46 +2,50 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+GREEN = '\033[92m'
+RED = '\033[91m'
+RESET = '\033[0m'
+
 def test_proxy(proxy):
-    url = 'http://google.com'
+    url = 'http://google.com'  
     proxies = {
         'http': f'http://{proxy}',
         'https': f'https://{proxy}'
     }
-    
+
     start_time = time.time()
-    
+
     try:
         response = requests.get(url, proxies=proxies, timeout=5)
         if response.status_code == 200:
             elapsed_time = time.time() - start_time
-            print(f"Proxy: {proxy}\tResponse Time: {elapsed_time:.2f} seconds")
+            print(f"{GREEN}Proxy: {proxy}\tResponse Time: {elapsed_time:.2f} seconds{RESET}")
         else:
-            print(f"Proxy: {proxy}\tResponse: {response.status_code}")
+            print(f"{RED}Proxy: {proxy}\tResponse: {response.status_code}{RESET}")
     except requests.exceptions.RequestException as e:
-        print(f"Proxy: {proxy}\tError: {e}")
+        print(f"{RED}Proxy: {proxy}\tError: {e}{RESET}")
 
 def scrape_proxies(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    
+
     proxy_list = []
-    
+
     table = soup.find('table')
     rows = table.find_all('tr')
-    
+
     for row in rows[1:]:
         columns = row.find_all('td')
         ip = columns[0].text
         port = columns[1].text
         proxy = f"{ip}:{port}"
         proxy_list.append(proxy)
-    
+
     return proxy_list
 
-# Example usage:
 url = 'https://www.sslproxies.org'
 proxies = scrape_proxies(url)
+
 
 for proxy in proxies:
     test_proxy(proxy)
